95e8abe44948d7b83ddcc423544c14b175ab5279
BetaZeon <ashwanitripathi144@gmail.com>
2020-06-01 01:07:46 +0530

    Updated JS and CSS file

--- pixel_art_maker/designs.js
+++ pixel_art_maker/designs.js
@@ -1,34 +1,33 @@
-"use strict";
 
-const $tableElement = $('#pixelCanvas');
-const $inputHeight = $('#inputHeight');
-const $inputWidth = $('#inputWidth');
 const $colorPicker = $('#colorPicker');
-// this sets the size of grid
-$('#sizePicker').submit( event => {
+// Select size input
+var h, w, p;
+// When size is submitted by the user, call createGrid()
+$("#sizePicker").submit(function (event) {
     event.preventDefault();
-
-    let width = $inputWidth.val();
-    let height = $inputHeight.val();
-
-    $tableElement.html(''); //clear
-
-    makeGrid(height, width);
+    h = $("#inputHeight").val();
+    w = $("#inputWidth").val();
+    createGrid(h, w);
     addCellClickListener();
-});
-/* It takes in height and width of the grid from the html form and creates
- * the grid on the table canvas element in html page.
- */
-function makeGrid(height, width) {
-    for(let i = 0; i < height; i++) {
-        $tableElement.append('<tr></tr>');
-    };
 
-    for(let i = 0; i < width; i++) {
-        $('tr').append('<td></td>');
-    };
-};
-//Function to change color of pixel when clicked.
+});
+
+// Creating table rows and table data
+function createGrid(h, w) {
+// remove current grid if one exists
+    $("tr").remove();
+for (var i = 1; i <= h; i++) {
+       $("#pixelCanvas").append("<tr></tr>");
+           for (var j = 0; j < w; j++) {
+             $('tr:last-child').append('<td></td>');
+             $('td').attr("class", 'color');
+           }
+    }
+}
+// Function to Paint td with color and also remove color
+// Adding and removing color to table data
+// When table data is clicked by a user, add background color or revert change to table data
+    
 function addCellClickListener() {
     $('td').click( event => {
         let color = $colorPicker.val();
